5d840e3b6dab86da08053a187f9a7b36bd250e38
Ali Nmb <n.ali75012@gmail.com>
2023-08-21 10:06:43 +0200

    Refacto: Divers modifications minimes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-        <h1 className="header__title">Chez vous, partout et ailleurs</h1>  
-        <h1 className="header__title2">Chez vous,<br/> partout et ailleurs</h1>  
+      <h1 className="header__title">Chez vous, partout et ailleurs</h1>
+      <h1 className="header__title2">
+        Chez vous,
+        <br /> partout et ailleurs
+      </h1>
     </header>
   );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import HeaderAbout from "../components/HeaderAbout";
 
 const About = () => {
   // const data = [
   //   {
   //     title: "Fiabilité",
-  //     content: "blablabla",
+  //     content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
   //   },
   //   {
   //     title: "Respect",
-  //     content: "blablabla",
+  //     content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
   //   },
   //   {
   //     title: "Service",
-  //     content: "blablabla",
+  //     content: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
   //   },
   //   {
   //     title: "Sécurité",
-  //     content: "blablabla",
+  //     content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
   //   },
   // ];
   return (
     <div className="about">
       <Navbar />
-      <div className="about__img"></div>
+      <HeaderAbout />
       <Footer />
     </div>
   );
